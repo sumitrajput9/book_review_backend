e4a0044fe4ebd635e6717726f8279ec2b6d1269c
Sumit Rajput <123628942+sumitrajput9@users.noreply.github.com>
2025-06-06 23:45:03 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -10,9 +10,20 @@ import reviewRoutes from './routes/review.routes';
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://book-review-backend-2-l59z.onrender.com',
+];
+
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   })
 );
